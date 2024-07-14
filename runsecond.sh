abe717099da48cffb75dc6408eb23954c03723a7
Tom Fieber <108031577+tomfieber@users.noreply.github.com>
2024-07-14 17:37:48 -0500

    Update runsecond.sh

--- runsecond.sh
+++ runsecond.sh
@@ -4,3 +4,5 @@ pipx install git+https://github.com/Pennyw0rth/NetExec
 
 cp th0m12.zsh-theme ~/.oh-my-zsh/themes/
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
