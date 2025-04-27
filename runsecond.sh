3bb1d6d3bf1ea31d2220affb8896472acdcd6fa7
Tom Fieber <108031577+tomfieber@users.noreply.github.com>
2025-04-27 09:17:10 -0500

    Update runsecond.sh

--- runsecond.sh
+++ runsecond.sh
@@ -7,4 +7,6 @@ cp th0m12.zsh-theme ~/.oh-my-zsh/themes/
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+pdtm -ia
+
 cp zshrc ~/.zshrc
