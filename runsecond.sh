6245ff206b61c370a8a5e1e86b62b5ffd0a72e2e
Tom Fieber <108031577+tomfieber@users.noreply.github.com>
2025-05-31 10:41:47 -0500

    Fix linking

--- runsecond.sh
+++ runsecond.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp th0m12.zsh-theme ~/.oh-my-zsh/themes/
+cp -r opt/.test_history /opt/ 
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
