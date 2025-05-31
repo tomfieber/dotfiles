94f673d191ecd95bff8d1d4dce1a0439bb972f51
Tom Fieber <108031577+tomfieber@users.noreply.github.com>
2025-05-31 17:04:02 -0500

    Fix logging

--- install-tools.sh
+++ install-tools.sh
@@ -83,6 +83,26 @@ install_snap() {
     return 0
 }
 
+install_go() {
+    local package=$1
+    log_info "Installing $package via go install"
+    if ! go install $package 2>>$LOG_FILE; then
+        log_error "Failed to install $package"
+        echo -e "${RED}Failed to install $package${NC}"
+        return 1
+    fi
+}
+
+install_pipx() {
+    local package=$1
+    log_info "Installing $package via pipx"
+    if ! sudo pipx install --global $package 2>>$LOG_FILE; then
+        log_error "Failed to install $package"
+        echo -e "${RED}Failed to install $package${NC}"
+        return 1
+    fi
+}
+
 # Install snap packages
 install_snap go --classic
 install_snap rustup --classic
@@ -118,7 +138,11 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 # Install Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+log_info "Installing Azure CLI"
+if ! curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash 2>>$LOG_FILE; then
+    log_error "Failed to install Azure CLI"
+    echo -e "${RED}Failed to install Azure CLI${NC}"
+fi
 
 # Install BloodHound Community Edition
 wget https://github.com/SpecterOps/bloodhound-cli/releases/latest/download/bloodhound-cli-linux-arm64.tar.gz
@@ -163,6 +187,7 @@ cd /opt/tools/john/src
 sudo make install
 
 # Drop a bunch of tools to /opt/tools
+log_info "Cloning various tools into /opt/tools"
 git clone https://github.com/bats3c/darkarmour.git /opt/tools/DarkAmour 
 git clone https://github.com/m0rtem/CloudFail.git /opt/tools/CloudFail
 git clone https://github.com/Ridter/noPac.git /opt/tools/noPac
@@ -195,24 +220,24 @@ git clone https://github.com/synacktiv/php_filter_chain_generator.git /opt/tools
 
 
 # Install Go tools
-go install github.com/projectdiscovery/pdtm/cmd/pdtm@latest
-go install github.com/owasp-amass/amass/v4/...@master
-go install github.com/ffuf/ffuf/v2@latest
-go install github.com/ropnop/kerbrute@latest
-go install github.com/sensepost/gowitness@latest
-go install github.com/RedTeamPentesting/pretender@latest
-go install github.com/EgeBalci/amber@latest
-go install github.com/tomnomnom/anew@latest
-go install github.com/asdf-vm/asdf/cmd/asdf@latest
-go install github.com/tomnomnom/assetfinder@latest
-go install github.com/lobuhi/byp4xx@latest
-go install github.com/lc/gau/v2/cmd/gau@latest
-go install github.com/hakluke/hakrawler@latest
-go install github.com/hakluke/hakrevdns@latest
-go install github.com/ipinfo/cli/ipinfo@latest
-go install github.com/BishopFox/jsluice/cmd/jsluice@latest
+install_go github.com/projectdiscovery/pdtm/cmd/pdtm@latest
+install_go github.com/owasp-amass/amass/v4/...@master
+install_go github.com/ffuf/ffuf/v2@latest
+install_go github.com/ropnop/kerbrute@latest
+install_go github.com/sensepost/gowitness@latest
+install_go github.com/RedTeamPentesting/pretender@latest
+install_go github.com/EgeBalci/amber@latest
+install_go github.com/tomnomnom/anew@latest
+install_go github.com/asdf-vm/asdf/cmd/asdf@latest
+install_go github.com/tomnomnom/assetfinder@latest
+install_go github.com/lobuhi/byp4xx@latest
+install_go github.com/lc/gau/v2/cmd/gau@latest
+install_go github.com/hakluke/hakrawler@latest
+install_go github.com/hakluke/hakrevdns@latest
+install_go github.com/ipinfo/cli/ipinfo@latest
+install_go github.com/BishopFox/jsluice/cmd/jsluice@latest
 curl -sSfL https://raw.githubusercontent.com/trufflesecurity/trufflehog/main/scripts/install.sh | sh -s -- -b /usr/local/bin
-go install github.com/tomnomnom/waybackurls@latest
+install_go github.com/tomnomnom/waybackurls@latest
 
 # Fix gau
 mv $HOME/go/bin/gau $HOME/go/bin/gau-cli 
@@ -226,63 +251,63 @@ make
 sudo ln -s /opt/tools/massdns/bin/massdns /usr/local/bin/massdns
 
 # Installing pipx tools
-sudo pipx install --global git+https://github.com/ly4k/Certipy.git
-sudo pipx install --global git+https://github.com/dirkjanm/BloodHound.py.git
-sudo pipx install --global git+https://github.com/blacklanternsecurity/MANSPIDER
-sudo pipx install --global git+https://github.com/Pennyw0rth/NetExec
-sudo pipx install --global git+https://github.com/p0dalirius/Coercer.git
-sudo pipx install --global git+https://github.com/skelsec/pypykatz.git
-sudo pipx install --global git+https://github.com/dirkjanm/mitm6.git
-sudo pipx install --global git+https://github.com/Mazars-Tech/AD_Miner.git
-sudo pipx install --global git+https://github.com/fortra/impacket.git
-sudo pipx install --global git+https://github.com/zblurx/certsync.git
-sudo pipx install --global git+https://github.com/ImpostorKeanu/parsuite.git
-sudo pipx install --global git+https://github.com/AetherBlack/abuseACL.git
-sudo pipx install --global git+https://github.com/aas-n/aclpwn.py.git
-sudo pipx install --global git+https://github.com/AD-Security/AD_Miner.git
-sudo pipx install --global git+https://github.com/dirkjanm/adidnsdump.git
-sudo pipx install --global git+https://github.com/androguard/androguard.git
-sudo pipx install --global git+https://github.com/angr/angr.git
-sudo pipx install --global git+https://github.com/s0md3v/Arjun.git
-sudo pipx install --global git+https://github.com/Orange-Cyberdefense/arsenal.git
-sudo pipx install --global git+https://github.com/dirkjanm/BloodHound.py.git
-sudo pipx install --global git+https://github.com/CravateRouge/bloodyAD.git
-sudo pipx install --global git+https://github.com/login-securite/conpass.git
-sudo pipx install --global git+https://github.com/chenjj/CORScanner.git
-sudo pipx install --global git+https://github.com/ihebski/DefaultCreds-cheat-sheet.git
-sudo pipx install --global git+https://github.com/maurosoria/dirsearch.git
-sudo pipx install --global git+https://github.com/login-securite/DonPAPI.git
-sudo pipx install --global git+https://github.com/zblurx/dploot.git
-sudo pipx install --global git+https://github.com/cddmp/enum4linux-ng.git
-sudo pipx install --global git+https://github.com/arthaud/git-dumper.git
-sudo pipx install --global git+https://github.com/Dramelac/GoldenCopy.git
-sudo pipx install --global git+https://github.com/khast3x/h8mail.git
-sudo pipx install --global git+https://github.com/almandin/krbjack.git
-sudo pipx install --global git+https://github.com/dirkjanm/ldapdomaindump.git
-sudo pipx install --global git+https://github.com/yaap7/ldapsearch-ad.git
-sudo pipx install --global git+https://github.com/bee-san/Name-That-Hash.git
-sudo pipx install --global git+https://github.com/codingo/NoSQLMap.git
-sudo pipx install --global git+https://github.com/i3visio/osrframework.git
-sudo pipx install --global git+https://github.com/aniqfakhrul/powerview.py.git
-sudo pipx install --global git+https://github.com/calebstewart/pwncat.git
-sudo pipx install --global git+https://github.com/Gallopsled/pwntools.git
-sudo pipx install --global git+https://github.com/skelsec/pysnaffler.git
-sudo pipx install --global git+https://github.com/the-useless-one/pywerview.git
-sudo pipx install --global git+https://github.com/ShutdownRepo/pywhisker.git
-sudo pipx install --global git+https://github.com/dirkjanm/ROADtools.git
-sudo pipx install --global git+https://github.com/Tw1sm/RITM.git
-sudo pipx install --global git+https://github.com/threat9/routersploit.git
-sudo pipx install --global git+https://github.com/garrettfoster13/sccmhunter.git
-sudo pipx install --global git+https://github.com/nccgroup/ScoutSuite.git
-sudo pipx install --global git+https://github.com/EnableSecurity/sipvicious.git
-sudo pipx install --global git+https://github.com/p0dalirius/smbclient-ng.git
-sudo pipx install --global git+https://github.com/jtesta/ssh-audit.git
-sudo pipx install --global git+https://github.com/sshuttle/sshuttle.git
-sudo pipx install --global git+https://github.com/aboul3la/Sublist3r.git
-sudo pipx install --global git+https://github.com/blacklanternsecurity/TREVORspray.git
-sudo pipx install --global git+https://github.com/sc0tfree/updog.git
-sudo pipx install --global git+https://github.com/EnableSecurity/wafw00f.git
-sudo pipx install --global git+https://github.com/garrettfoster13/pre2k.git
+install_pipx git+https://github.com/ly4k/Certipy.git
+install_pipx git+https://github.com/dirkjanm/BloodHound.py.git
+install_pipx git+https://github.com/blacklanternsecurity/MANSPIDER
+install_pipx git+https://github.com/Pennyw0rth/NetExec
+install_pipx git+https://github.com/p0dalirius/Coercer.git
+install_pipx git+https://github.com/skelsec/pypykatz.git
+install_pipx git+https://github.com/dirkjanm/mitm6.git
+install_pipx git+https://github.com/Mazars-Tech/AD_Miner.git
+install_pipx git+https://github.com/fortra/impacket.git
+install_pipx git+https://github.com/zblurx/certsync.git
+install_pipx git+https://github.com/ImpostorKeanu/parsuite.git
+install_pipx git+https://github.com/AetherBlack/abuseACL.git
+install_pipx git+https://github.com/aas-n/aclpwn.py.git
+install_pipx git+https://github.com/AD-Security/AD_Miner.git
+install_pipx git+https://github.com/dirkjanm/adidnsdump.git
+install_pipx git+https://github.com/androguard/androguard.git
+install_pipx git+https://github.com/angr/angr.git
+install_pipx git+https://github.com/s0md3v/Arjun.git
+install_pipx git+https://github.com/Orange-Cyberdefense/arsenal.git
+install_pipx git+https://github.com/dirkjanm/BloodHound.py.git
+install_pipx git+https://github.com/CravateRouge/bloodyAD.git
+install_pipx git+https://github.com/login-securite/conpass.git
+install_pipx git+https://github.com/chenjj/CORScanner.git
+install_pipx git+https://github.com/ihebski/DefaultCreds-cheat-sheet.git
+install_pipx git+https://github.com/maurosoria/dirsearch.git
+install_pipx git+https://github.com/login-securite/DonPAPI.git
+install_pipx git+https://github.com/zblurx/dploot.git
+install_pipx git+https://github.com/cddmp/enum4linux-ng.git
+install_pipx git+https://github.com/arthaud/git-dumper.git
+install_pipx git+https://github.com/Dramelac/GoldenCopy.git
+install_pipx git+https://github.com/khast3x/h8mail.git
+install_pipx git+https://github.com/almandin/krbjack.git
+install_pipx git+https://github.com/dirkjanm/ldapdomaindump.git
+install_pipx git+https://github.com/yaap7/ldapsearch-ad.git
+install_pipx git+https://github.com/bee-san/Name-That-Hash.git
+install_pipx git+https://github.com/codingo/NoSQLMap.git
+install_pipx git+https://github.com/i3visio/osrframework.git
+install_pipx git+https://github.com/aniqfakhrul/powerview.py.git
+install_pipx git+https://github.com/calebstewart/pwncat.git
+install_pipx git+https://github.com/Gallopsled/pwntools.git
+install_pipx git+https://github.com/skelsec/pysnaffler.git
+install_pipx git+https://github.com/the-useless-one/pywerview.git
+install_pipx git+https://github.com/ShutdownRepo/pywhisker.git
+install_pipx git+https://github.com/dirkjanm/ROADtools.git
+install_pipx git+https://github.com/Tw1sm/RITM.git
+install_pipx git+https://github.com/threat9/routersploit.git
+install_pipx git+https://github.com/garrettfoster13/sccmhunter.git
+install_pipx git+https://github.com/nccgroup/ScoutSuite.git
+install_pipx git+https://github.com/EnableSecurity/sipvicious.git
+install_pipx git+https://github.com/p0dalirius/smbclient-ng.git
+install_pipx git+https://github.com/jtesta/ssh-audit.git
+install_pipx git+https://github.com/sshuttle/sshuttle.git
+install_pipx git+https://github.com/aboul3la/Sublist3r.git
+install_pipx git+https://github.com/blacklanternsecurity/TREVORspray.git
+install_pipx git+https://github.com/sc0tfree/updog.git
+install_pipx git+https://github.com/EnableSecurity/wafw00f.git
+install_pipx git+https://github.com/garrettfoster13/pre2k.git
 
 # Install sliver 
 curl https://sliver.sh/install|sudo bash
